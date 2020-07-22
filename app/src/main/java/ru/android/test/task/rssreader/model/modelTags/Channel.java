c517b097838e6be0356fbb268f4669adaec3a679
Yulia Krasnik <i.krasnik@g.nsu.ru>
2020-07-22 20:21:35 +0700

    add rss feed model

---
+++ app/src/main/java/ru/android/test/task/rssreader/model/modelTags/Channel.java
@@ -0,0 +1,15 @@
+package ru.android.test.task.rssreader.model.modelTags;
+
+import org.simpleframework.xml.Element;
+import org.simpleframework.xml.ElementList;
+import org.simpleframework.xml.Root;
+
+import java.util.List;
+
+@Root(name = "channel", strict = false)
+public class Channel {
+    @ElementList(inline = true, required = false)
+    private List<Item> item;
+    @Element
+    private String title;
+}
