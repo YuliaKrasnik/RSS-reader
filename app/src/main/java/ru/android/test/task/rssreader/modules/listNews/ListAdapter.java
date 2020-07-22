c0e7d1f12e4354176fac7e9f35c759a8958bcb84
Yulia Krasnik <i.krasnik@g.nsu.ru>
2020-07-23 01:30:13 +0700

    realize list adapter and view holder

--- app/src/main/java/ru/android/test/task/rssreader/modules/listNews/ListAdapter.java
+++ app/src/main/java/ru/android/test/task/rssreader/modules/listNews/ListAdapter.java
@@ -1,24 +1,42 @@
 package ru.android.test.task.rssreader.modules.listNews;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class ListAdapter extends RecyclerView.Adapter <ListViewHolder> {
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import ru.android.test.task.rssreader.R;
+import ru.android.test.task.rssreader.model.modelDb.News;
+
+public class ListAdapter extends RecyclerView.Adapter<ListViewHolder> {
+    private final List<News> listNews;
+
+    public ListAdapter(List<News> listNews) {
+        this.listNews = listNews;
+    }
+
     @NonNull
     @Override
     public ListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_news, parent, false);
+        return new ListViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ListViewHolder holder, int position) {
-
+        holder.getTitleNews().setText(listNews.get(position).title);
+        holder.getDateNews().setText(listNews.get(position).pubDate);
+        Picasso.get().load(listNews.get(position).photoNews.url).into(holder.getImageNews());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listNews.size();
     }
 }
--- app/src/main/java/ru/android/test/task/rssreader/modules/listNews/ListViewHolder.java
+++ app/src/main/java/ru/android/test/task/rssreader/modules/listNews/ListViewHolder.java
@@ -1,12 +1,37 @@
 package ru.android.test.task.rssreader.modules.listNews;
 
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class ListViewHolder extends RecyclerView.ViewHolder  {
-    public ListViewHolder(@NonNull View itemView) {
+import ru.android.test.task.rssreader.R;
+
+public class ListViewHolder extends RecyclerView.ViewHolder {
+    private TextView titleNews;
+    private TextView dateNews;
+    private ImageView imageNews;
+
+    ListViewHolder(@NonNull View itemView) {
         super(itemView);
+        titleNews = itemView.findViewById(R.id.txtTitle);
+        dateNews = itemView.findViewById(R.id.txtPubDate);
+        imageNews = itemView.findViewById(R.id.imageView);
     }
+
+    TextView getTitleNews() {
+        return titleNews;
+    }
+
+    TextView getDateNews() {
+        return dateNews;
+    }
+
+    ImageView getImageNews() {
+        return imageNews;
+    }
+
 }
