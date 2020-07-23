e510d6a4edae09830d905fc9979ef63cbfa9a311
Yulia Krasnik <i.krasnik@g.nsu.ru>
2020-07-23 13:14:56 +0700

    create layout profile

---
+++ app/src/main/java/ru/android/test/task/rssreader/modules/profileNews/ProfileNewsActivity.java
@@ -0,0 +1,16 @@
+package ru.android.test.task.rssreader.modules.profileNews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ru.android.test.task.rssreader.R;
+
+public class ProfileNewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_news);
+    }
+}
